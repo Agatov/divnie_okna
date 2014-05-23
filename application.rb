17e407749542590e9372ea9a48299d20c4c92819
agatov <agatovs@gmail.com>
2014-05-23 15:30:04 +0400

    initial commit

---
+++ application.rb
@@ -0,0 +1,59 @@
+require 'sinatra/base'
+require 'sinatra/assetpack'
+require 'haml'
+require 'sass'
+require 'httparty'
+require 'json'
+require 'pony'
+require 'i18n'
+
+
+I18n.load_path += Dir[File.join(File.dirname(__FILE__), 'config', 'locales', '*.yml').to_s]
+
+class Application < Sinatra::Base
+  set :root, File.dirname(__FILE__)
+  set :sass, { :load_paths => [ "#{Application.root}/assets/stylesheets" ] }
+  set :protection, :except => :frame_options
+
+  register Sinatra::AssetPack
+
+  assets {
+    serve '/css', from: 'assets/stylesheets'
+    serve '/images', from: 'assets/images'
+    serve '/js', from: 'assets/javascripts'
+    serve '/fonts', from: 'assets/fonts'
+
+    css :application, '/css/application.css', %w(/css/main.css /css/jquery.fancybox.css)
+    js :application, '/js/application.js', %w( /js/jquery.carouFredSel-5.2.2-packed.js /js/jquery.fancybox.js /js/jquery.xfade-1.0.min.js /js/organictabs.jquery.js /js/required_fields.js )
+
+    css_compression :sass
+    js_compression :jsmin
+  }
+
+  get '/' do
+    erb :index, layout: :layout
+  end
+
+  post '/orders.json' do
+
+    message = "#{params[:order][:username]}. #{params[:order][:phone]}. #{params[:order][:email]}"
+
+    #Pony.mail ({
+    #    to: 'vsedlyavolvo@yandex.ru',
+    #    subject: I18n.t('email.title', locale: 'ru'),
+    #    body: message,
+    #    via: :smtp,
+    #    via_options: {
+    #        address: 'smtp.gmail.com',
+    #        port: 587,
+    #        enable_starttls_auto: true,
+    #        user_name: 'vsedlyavolvo',
+    #        password: 'moyparol',
+    #        authentication: :plain
+    #    }
+    #})
+
+    content_type :json
+    {status: :success}.to_json
+  end
+end
---
+++ assets/javascripts/required_fields.js
@@ -0,0 +1,127 @@
+(function( $ ){
+
+	$(function() {
+		$('#form1').each(function(){
+			var form = $(this),
+				btn = form.find('.btn_submit1');
+			
+			form.find('.rfield').addClass('empty_field');
+			
+			// Функция проверки полей формы
+			function checkInput(){
+				form.find('.rfield').each(function(){
+					if($(this).val() != ''){
+						$(this).removeClass('empty_field');
+					} else {
+						$(this).addClass('empty_field');
+					}
+				});
+			}
+			
+			// Функция подсветки незаполненных полей
+			function lightEmpty(){
+				form.find('.empty_field').css({'border-color':'#d8512d'});
+				setTimeout(function(){
+					form.find('.empty_field').removeAttr('style');
+				},500);
+			}
+			
+			setInterval(function(){
+				checkInput();
+				var sizeEmpty = form.find('.empty_field').size();
+				if(sizeEmpty > 0){
+					if(btn.hasClass('disabled')){
+						return false
+					} else {
+						btn.addClass('disabled')
+					}
+				} else {
+					btn.removeClass('disabled')
+				}
+			},500);
+
+			btn.click(function(){
+				if($(this).hasClass('disabled')){
+					lightEmpty();
+					return false
+				} else {
+					$.ajax({
+            type: 'POST',
+            url: '/orders.json',
+            data: $('#form1').serialize(),
+            success: function () {
+			  	$("#form1 #name").val("");
+				$("#form1 #phone").val("");
+				$("#form1 #date").val("");
+                alert('Спасибо! Ваша заявка отправлена.');
+            }
+          });
+				}
+			});
+			
+		});
+		
+		
+		$('#form2').each(function(){
+			var form = $(this),
+				btn = form.find('.btn_submit2');
+			
+			form.find('.rfield').addClass('empty_field');
+			
+			// Функция проверки полей формы
+			function checkInput(){
+				form.find('.rfield').each(function(){
+					if($(this).val() != ''){
+						$(this).removeClass('empty_field');
+					} else {
+						$(this).addClass('empty_field');
+					}
+				});
+			}
+			
+			// Функция подсветки незаполненных полей
+			function lightEmpty(){
+				form.find('.empty_field').css({'border-color':'#d8512d'});
+				setTimeout(function(){
+					form.find('.empty_field').removeAttr('style');
+				},500);
+			}
+			
+			setInterval(function(){
+				checkInput();
+				var sizeEmpty = form.find('.empty_field').size();
+				if(sizeEmpty > 0){
+					if(btn.hasClass('disabled')){
+						return false
+					} else {
+						btn.addClass('disabled')
+					}
+				} else {
+					btn.removeClass('disabled')
+				}
+			},500);
+
+			btn.click(function(){
+				if($(this).hasClass('disabled')){
+					lightEmpty();
+					return false
+				} else {
+					$.ajax({
+            type: 'POST',
+            url: '/orders.json',
+            data: $('#form2').serialize(),
+            success: function () {
+			  	$("#form2 #name").val("");
+				$("#form2 #phone").val("");
+				$("#form2 #date").val("");
+                alert('Спасибо! Ваша заявка отправлена.');
+            }
+          });
+				}
+			});
+			
+		});
+		
+	});
+
+})( jQuery );
