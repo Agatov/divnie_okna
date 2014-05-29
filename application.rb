b7df766b39c591fad256e11da3ac2939e9e35cbe
agatov <agatovs@gmail.com>
2014-05-29 13:12:40 +0400

    fix

--- application.rb
+++ application.rb
@@ -36,7 +36,7 @@ class Application < Sinatra::Base
 
   post '/orders.json' do
 
-    message = "#{params[:order][:username]}. #{params[:order][:phone]}. #{params[:order][:date]}"
+    message = "#{params[:name][:username]}. #{params[:order][:phone]}. #{params[:order][:date]}"
 
     Pony.mail ({
         to: 'abardacha@gmail.com, mkt3@divnie.ru',
